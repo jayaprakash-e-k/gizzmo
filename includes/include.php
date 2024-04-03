24800c8ed7593a4e00a949fa1e5c57fcddb068e8
JAYAPRAKASH E K <jp008882@gmail.com>
2024-04-03 15:30:31 +0530

    Commit on 03/04

--- includes/include.php
+++ includes/include.php
@@ -1,9 +1,9 @@
 <?php
-    $hostURL = (isset($_SERVER['HTTPS'])) ? "https" : "http" . "://" . $_SERVER['SERVER_NAME'];
+    $hostURL = ((isset($_SERVER['HTTPS'])) ? "https" : "http" . "://") . $_SERVER['SERVER_NAME'];
 ?>
 
 <!-- Style Sheet -->
-<link rel="stylesheet" type="text/css" href="<?php echo $hostURL; ?>/gizzmo/assets/css/style.css?<?php echo time(); ?>" />
+<link rel="stylesheet" type="text/css" href="./assets/css/style.css?<?php echo time(); ?>" />
 
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -12,7 +12,7 @@
 <script src="https://kit.fontawesome.com/ef6be5e094.js" crossorigin="anonymous"></script>
 
 <!-- Icon -->
-<link rel="icon" href="<?php echo $hostURL; ?>/gizzmo/assets/img/logo.png" type="image/png" />
+<link rel="icon" href="./assets/img/logo.png" type="image/png" />
 
 <!-- JS Files -->
-<script src="<?php echo $hostURL; ?>/gizzmo/assets/js/message.js?<?php echo time(); ?>" type="text/javascript"></script>
+<script src="./assets/js/message.js?<?php echo time(); ?>" type="text/javascript"></script>
